e5a05dd419121ef02328dda6a63c0543a67115cb
Miguel-Venegas <venegas.a.miguel@gmail.com>
2020-12-29 11:22:01 -0800

    function that returns a random playing card as an object

---
+++ app.js
@@ -0,0 +1,23 @@
+// Write a getCard() function which returns a random playing card object, like:
+// 		{
+// 			value: 'K'
+// 			suit: 'clubs'
+// 		}
+//Pick a random value from:
+//----1,2,3,4,5,6,7,8,9,10,J,Q,K,A
+//Pick a random suit from:
+//----clubs,spades, hearts, diamonds
+//Return both in an object
+
+
+
+function getCard(){
+    const char = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const suit = ['clubs', 'spades', 'hearts', 'diamonds'];
+
+    let randomChar = Math.floor(Math.random() * char.length);
+    let randomSuit = Math.floor(Math.random() * suit.length);
+
+    return {value: char[randomChar], suit: suit[randomSuit]};
+}
+
